72c975b69404cade1544fb0b8cf7daeb98d0f523
Sainzaya Batkhuu <sainzaya@Sainzayas-MacBook-Pro.local>
2020-05-22 18:33:54 +0800

    update project

--- src/app/pages/student/tests/tests.component.ts
+++ src/app/pages/student/tests/tests.component.ts
@@ -12,13 +12,63 @@ import { faFilm } from '@fortawesome/free-solid-svg-icons';
 })
 export class TestsComponent implements OnInit {
   cateroties = [];
+  currLng = "";
   filmIcon = faFilm;
 
   constructor(private titleService: Title,private meta: Meta,private activatedRoute: ActivatedRoute,private api: ApiService) {
     this.titleService.setTitle( 'Tests - localhost' );
     this.meta.addTag({ name: 'description', content: 'An online application for typing fast with ten fingers.' });
+    this.currLng = "ENGLISH";
     this.api.get('/api/studentlessons').subscribe(response => {
-      this.cateroties = response;
+      this.cateroties = [
+        {
+          enTitle:"Timed Tests",
+          mnTitle:"Хугацаатай Сорилтууд",
+          slug:"timed-tests",
+          lessons: [{
+            enTitle:"1:00 Test",
+            mnTitle:"1:00 Сорилт",
+            slug:"1-minute",
+            enBttnLbl:"1 Minute Typing Test",
+            mnBttnLbl:"1 Минут Бичих Сорилт"
+          },{
+            enTitle:"3:00 Test",
+            mnTitle:"3:00 Сорилт",
+            slug:"3-minute",
+            enBttnLbl:"3 Minute Typing Test",
+            mnBttnLbl:"3 Минут Бичих Сорилт"
+          },{
+            enTitle:"5:00 Test",
+            mnTitle:"5:00 Сорилт",
+            slug:"5-minute",
+            enBttnLbl:"5 Minute Typing Test",
+            mnBttnLbl:"5 Минут Бичих Сорилт"
+          }]
+        },{
+          enTitle:"Page Tests",
+          mnTitle:"Хуудасны Сорилтууд",
+          slug:"page-tests",
+          lessons: [{
+            enTitle:"1 Page Test",
+            mnTitle:"1 Хуудасны Сорилт",
+            slug:"1-page",
+            enBttnLbl:"1 Page Typing Test",
+            mnBttnLbl:"1 Хуудасны Бичих Сорилт"
+          },{
+            enTitle:"2 Page Test",
+            mnTitle:"2 Хуудасны Сорилт",
+            slug:"2-page",
+            enBttnLbl:"2 Page Typing Test",
+            mnBttnLbl:"2 Хуудасны Бичих Сорилт"
+          },{
+            enTitle:"3 Page Test",
+            mnTitle:"3 Хуудасны Сорилт",
+            slug:"3-page",
+            enBttnLbl:"3 Page Typing Test",
+            mnBttnLbl:"3 Хуудасны Бичих Сорилт"
+          }]
+        }
+      ];
     })
   }
 
@@ -26,10 +76,7 @@ export class TestsComponent implements OnInit {
   }
 
   getLessons(langauge: string) {
-    console.log(langauge);
-    this.api.get('/api/studentlessons').subscribe(response => {
-      console.log(response);
-    })
+    this.currLng = langauge;
   }
 
 }
--- src/app/pages/student/typing-test/typing-test-routing.module.ts
+++ src/app/pages/student/typing-test/typing-test-routing.module.ts
@@ -4,7 +4,7 @@ import { TypingTestComponent } from './typing-test.component';
 
 
 const routes: Routes = [{
-  path: ':slug',
+  path: '',
   component: TypingTestComponent
 }];
 

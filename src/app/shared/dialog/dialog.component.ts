71caa50ac63759bf409ed01eab213adc41672453
Goodaway <sainzaya.b@gmail.com>
2022-09-21 18:55:27 +0800

    Update dialog.component.ts

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent implements OnInit {
+export class DialogComponent implements OnInit, OnDestroy {
 
+  @Input()
+  isClose:boolean = false;
+  @Input()
+  hasBgLayout:boolean = true;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
+  onOverlayClicked(evt: MouseEvent): void {
+
+  }
+
+  onDialogClicked(evt: MouseEvent): void {
+    evt.stopPropagation();
   }
 
 }
